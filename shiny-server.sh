dafa5885afb3d3a0a1f84655918ace1fb39fdd6b
Yvan Le Bras <yvan.le-bras@mnhn.fr>
2022-06-18 14:00:45 +0200

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -4,7 +4,8 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
-chmod +x /monitor_traffic.sh
-exec /monitor_traffic.sh &
+# from GIE not to use with GxIT IIANW
+# chmod +x /monitor_traffic.sh
+# exec /monitor_traffic.sh &
 
 exec shiny-server 2>&1
